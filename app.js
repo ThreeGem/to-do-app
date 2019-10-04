ea14be27047a6c2aaabac21c1b4a5fb6c2834d9d
Simeng <simengzeng11@gmail.com>
2019-10-04 15:41:55 -0700

    added app.js

---
+++ app.js
@@ -0,0 +1,45 @@
+
+window.onload = function() {
+  onReady();
+};
+
+function onReady() {
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+
+  addToDoForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      let title = newToDoText.value;
+
+      let newLi = document.createElement('li');
+
+      let checkbox = document.createElement('input');
+
+      checkbox.type = "checkbox";
+
+      let deleteBtn = document.createElement('button');
+
+      deleteBtn.textContent = "Delete";
+
+      deleteBtn.addEventListener('click', function(event){
+
+        toDoList.removeChild(this.parentElement);
+      })
+
+
+      newLi.textContent = title;
+
+      newLi.appendChild(checkbox);
+
+      newLi.appendChild(deleteBtn);
+
+      toDoList.appendChild(newLi);
+
+      newToDoText.value = '';
+
+
+  });
+
+}
